d66dacc711e1d6caf31353c69ac5edaa7587237a
jazferlex <79888058+jazferlex@users.noreply.github.com>
2022-09-12 11:26:36 +0800

    Updated models.py

--- wb2/index/models.py
+++ wb2/index/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
@@ -20,4 +21,48 @@ class Rates(models.Model):
     rateAfterMin = models.IntegerField()
     ratePenalty = models.IntegerField()
     ratePenaltyFreq = models.IntegerField()
+
+class BarangayRecord(models.Model):
+    B_RecordID = models.IntegerField()
+    barangaycode = models.IntegerField()
+    year = models.IntegerField()
+    total_due_jan = models.IntegerField()
+    total_paid_jan = models.IntegerField()
+    total_usage_jan = models.IntegerField()
+    total_due_feb = models.IntegerField()
+    total_paid_feb = models.IntegerField()
+    total_usage_feb = models.IntegerField()
+    total_due_mar = models.IntegerField()
+    total_paid_mar = models.IntegerField()
+    total_usage_mar = models.IntegerField()
+    total_due_apr = models.IntegerField()
+    total_paid_apr = models.IntegerField()
+    total_usage_apr = models.IntegerField()
+    total_due_may = models.IntegerField()
+    total_paid_may = models.IntegerField()
+    total_usage_may = models.IntegerField()
+    total_due_jun = models.IntegerField()
+    total_paid_jun = models.IntegerField()
+    total_usage_jun = models.IntegerField()
+    total_due_jul = models.IntegerField()
+    total_paid_jul = models.IntegerField()
+    total_usage_jul = models.IntegerField()
+    total_due_aug = models.IntegerField()
+    total_paid_aug = models.IntegerField()
+    total_usage_aug = models.IntegerField()
+    total_due_sept = models.IntegerField()
+    total_paid_sept = models.IntegerField()
+    total_usage_sept = models.IntegerField()
+    total_due_oct = models.IntegerField()
+    total_paid_oct = models.IntegerField()
+    total_usage_oct = models.IntegerField()
+    total_due_nov = models.IntegerField()
+    total_paid_nov = models.IntegerField()
+    total_usage_nov = models.IntegerField()
+    total_due_dec = models.IntegerField()
+    total_paid_dec = models.IntegerField()
+    total_usage_dec = models.IntegerField()
+
+
+
 
