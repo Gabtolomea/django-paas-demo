1558c7902ef09f164f4122fce80677f633aace08
jefreybiadnes <jefrey.biadnes@ctu.edu.ph>
2022-09-12 10:56:09 +0800

    created models.py

--- wb2/index/models.py
+++ wb2/index/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class SystemUsers(AbstractUser):
+    USER_TYPE = (
+        ('',''),
+    )
+    midname = models.CharField(max_length=20, blank=True)
+    lastname = models.CharField(max_length=20, blank=True)
+    mobilenum = models.CharField(max_length=20, blank=True)
+    usertype = models.CharField(max_length=20, choices=USER_TYPE)
+    profilepic = models.ImageField(blank=True, null=True)
+
+class Rates(models.Model):
+    minReading = models.IntegerField()
+    minReadingCharge = models.IntegerField()
+    rateAfterMin = models.IntegerField()
+    ratePenalty = models.IntegerField()
+    ratePenaltyFreq = models.IntegerField()
+
