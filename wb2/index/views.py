7fb3576d7617548b84ad68090cc2c36a3a0c30a2
jefreybiadnes <79888058+jazferlex@users.noreply.github.com>
2022-09-13 13:55:24 +0800

    started data input

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -9,15 +9,38 @@ from django.core.mail import EmailMessage
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 
+import mysql.connector
+
 from wb2 import settings
 from .forms import *
 from .decorators import *
 from .models import *
 
-
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="yjh434ctuG@-@",
+    database="lgu_ginatilan_db"
+)
 def landingpage(request):
-    context = {}
-    return render(request, 'index.html', context)
+    tablenames = ['accountinfo','accountrecord','barangay_record','consumerinfo','gettotalbill','meterreadingmodification_table','oldconsumerinfo','payment_history','ratestable','revenuecode','systemuser','yearly_records']
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM accountinfo")
+
+    myresult = mycursor.fetchall()
+    accountinfoid = []
+    for x in myresult:
+        accountinfoid.append(x[0])
+    for t in tablenames:
+        tablename = t
+
+    context = {
+        'accountinfoid':accountinfoid,
+    }
+
+    return render(request, 'home.html', context)
 
 
 def login(request):
