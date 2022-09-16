942cffeacdf8cc6fbe5981ea4c763f6862175706
jazferlex <79888058+jazferlex@users.noreply.github.com>
2022-09-16 13:23:37 +0800

    asdf

--- wb2/index/urls.py
+++ wb2/index/urls.py
@@ -6,7 +6,7 @@ from django.views.generic.base import RedirectView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',views.landingpage,name='lp'),
+    path('',views.porter_in,name='p_in'),
     path('login',views.login,name='login'),
     path('user_creation', views.user_creation,name='user_creation')
     
