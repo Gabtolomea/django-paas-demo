07f0abecfc029232945030e5214351e6167756de
jazferlex <91324862+chrlhjra@users.noreply.github.com>
2022-09-16 10:08:02 +0800

    da

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -1186,7 +1186,7 @@ def landingpage(request):
     for t in range(len(tablenames)):
         mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = "+tablenames[t]+";")
         for c in range(mycursor.fetchone()[0]):
-            mycursor.execute("SELECT " + columnnames[cols] + " FROM "+tablenames[t]+";")
+            mycursor.execute("SELECT " + columnnames[cols] + " FROM "+tablenames2[t]+";")
             alltables[t][c]=mycursor.fetchall()
             cols+=1
     
