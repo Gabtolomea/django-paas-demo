ddbc2847fee14eb3d0ff73671542a1118dc3dde0
jazferlex <79888058+jazferlex@users.noreply.github.com>
2022-09-16 14:23:27 +0800

    dataporter out working

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -1,4 +1,4 @@
-from multiprocessing import context
+
 from os import system
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -20,19 +20,6 @@ from .models import *
 
 
 tablenames = [
-    "'accountinfo'",
-    "'accountrecord'",
-    "'barangay_record'",
-    "'consumerinfo'",
-    "'gettotalbill'",
-    "'oldconsumerinfo'",
-    "'payment_history'",
-    "'ratestable'",
-    "'revenuecode'",
-    "'systemuser'",
-    "'yearly_records'"
-]
-tablenames2 = [
     "accountinfo",
     "accountrecord",
     "barangay_record",
@@ -1173,6 +1160,7 @@ alltables = [
     yearly_records,
 ]
 
+current_table = []
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -1183,40 +1171,74 @@ mycursor = mydb.cursor()
 def porter_in(request):
     col = 0
     for t in range(len(tablenames)):
-        mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = "+tablenames[t]+";")
+        mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+tablenames[t]+"';")
         for c in range(mycursor.fetchone()[0]):
-            mycursor.execute("SELECT " + columnnames[col] + " FROM "+tablenames2[t]+";")
+            mycursor.execute("SELECT " + columnnames[col] + " FROM "+tablenames[t]+";")
             result=mycursor.fetchall()
             for x in result:
                 alltables[t][c].append(x[0])
             col+=1
-    context = {
-        # 'accountrecord':accountrecord,
-        # 'barangay_record':barangay_record,
-        # 'consumerinfo':consumerinfo,
-        # 'gettotalbill':gettotalbill,
-        # 'meterreadingmodification_table':meterreadingmodification_table,
-        # 'oldconsumerinfo':oldconsumerinfo,
-        # 'payment_history':payment_history,
-        # 'ratestable':ratestable,
-        # 'revenuecode':revenuecode,
-        # 'systemuser':systemuser,
-        # 'yearly_records':yearly_records,
-    }
-
-    return render(request, 'home.html', context)
+    
+    current_table = rearrange('barangay_record')
+    porter_out(current_table)
+    return render(request, "home.html")
 
 def rearrange(var):
     arr = []
-    mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = 'barangay_record';")
+    mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+var+"';")
     cols = mycursor.fetchone()[0]
-    for r in range(98):
+    mycursor.execute("SELECT count(*) FROM "+var+";")
+    rows = mycursor.fetchone()[0]
+    for r in range(rows):
         inner = []
         for c in range(cols):
-            inner.append(barangay_record[c][r])
+            inner.append(globals()[var][c][r])
         arr.append(inner)
-    print(arr)
 
+    return arr
+
+def porter_out(table):
+    b_rec = BarangayRecord()
+    for i in table:
+        b_rec.barangay_val = i[0]
+        b_rec.year = i[2]
+        b_rec.total_due_jan = i[6]
+        b_rec.total_paid_jan = i[7]
+        b_rec.total_usage_jan = i[8]
+        b_rec.total_due_feb = i[9]
+        b_rec.total_paid_feb = i[10]
+        b_rec.total_usage_feb = i[11]
+        b_rec.total_due_mar = i[12]
+        b_rec.total_paid_mar = i[13]
+        b_rec.total_usage_mar = i[14]
+        b_rec.total_due_apr = i[15]
+        b_rec.total_paid_apr = i[16]
+        b_rec.total_usage_apr = i[17]
+        b_rec.total_due_may = i[18]
+        b_rec.total_paid_may = i[19]
+        b_rec.total_usage_may = i[20]
+        b_rec.total_due_jun = i[21]
+        b_rec.total_paid_jun = i[22]
+        b_rec.total_usage_jun = i[23]
+        b_rec.total_due_jul = i[24]
+        b_rec.total_paid_jul = i[25]
+        b_rec.total_usage_jul = i[26]
+        b_rec.total_due_aug = i[27]
+        b_rec.total_paid_aug = i[28]
+        b_rec.total_usage_aug = i[29]
+        b_rec.total_due_sept = i[30]
+        b_rec.total_paid_sept = i[31]
+        b_rec.total_usage_sept = i[32]
+        b_rec.total_due_oct = i[33]
+        b_rec.total_paid_oct = i[34]
+        b_rec.total_usage_oct = i[35]
+        b_rec.total_due_nov = i[36]
+        b_rec.total_paid_nov = i[37]
+        b_rec.total_usage_nov = i[38]
+        b_rec.total_due_dec = i[39]
+        b_rec.total_paid_dec = i[40]
+        b_rec.total_usage_dec = i[41]
+        b_rec.save()
 def login(request):
     if request.method == "POST":
         username = request.POST['username']
