053dcfeeebf2159ed0ffd8f3a88341f0340b5622
WelfyRobin <113080568+WelfyRobin@users.noreply.github.com>
2022-09-16 15:08:05 +0800

    09-16-2022

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -17,18 +17,6 @@ from .forms import *
 from .decorators import *
 from .models import *
 
-<<<<<<< HEAD
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="yjh434ctuG@-@",
-    database="lgu_ginatilan_db"
-)
-
-
-def landingpage(request):
-=======
-
 
 def landingpage(request):
     mydb = mysql.connector.connect(
@@ -37,7 +25,7 @@ def landingpage(request):
         password="yjh434ctuG@-@",
         database="lgu_ginatilan_db"
     )
->>>>>>> e95558786a135c5bbeaf98cee8863d269d599045
+
     tablenames = [
         "accountinfo",
         "accountrecord",
