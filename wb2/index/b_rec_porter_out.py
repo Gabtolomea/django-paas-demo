41590cf09ce692ac6ca97938405fd1b481f844d1
jazferlex <79888058+jazferlex@users.noreply.github.com>
2022-09-17 09:48:24 +0800

    adfadf

--- wb2/index/b_rec_porter_out.py
+++ wb2/index/b_rec_porter_out.py
@@ -1,43 +1,96 @@
-from .models import *
-def porter_out(table):
-    b_rec = BarangayRecord()
-    for i in table:
-        b_rec.barangay_val = i[0]
-        b_rec.year = i[2]
-        b_rec.total_due_jan = i[6]
-        b_rec.total_paid_jan = i[7]
-        b_rec.total_usage_jan = i[8]
-        b_rec.total_due_feb = i[9]
-        b_rec.total_paid_feb = i[10]
-        b_rec.total_usage_feb = i[11]
-        b_rec.total_due_mar = i[12]
-        b_rec.total_paid_mar = i[13]
-        b_rec.total_usage_mar = i[14]
-        b_rec.total_due_apr = i[15]
-        b_rec.total_paid_apr = i[16]
-        b_rec.total_usage_apr = i[17]
-        b_rec.total_due_may = i[18]
-        b_rec.total_paid_may = i[19]
-        b_rec.total_usage_may = i[20]
-        b_rec.total_due_jun = i[21]
-        b_rec.total_paid_jun = i[22]
-        b_rec.total_usage_jun = i[23]
-        b_rec.total_due_jul = i[24]
-        b_rec.total_paid_jul = i[25]
-        b_rec.total_usage_jul = i[26]
-        b_rec.total_due_aug = i[27]
-        b_rec.total_paid_aug = i[28]
-        b_rec.total_usage_aug = i[29]
-        b_rec.total_due_sept = i[30]
-        b_rec.total_paid_sept = i[31]
-        b_rec.total_usage_sept = i[32]
-        b_rec.total_due_oct = i[33]
-        b_rec.total_paid_oct = i[34]
-        b_rec.total_usage_oct = i[35]
-        b_rec.total_due_nov = i[36]
-        b_rec.total_paid_nov = i[37]
-        b_rec.total_usage_nov = i[38]
-        b_rec.total_due_dec = i[39]
-        b_rec.total_paid_dec = i[40]
-        b_rec.total_usage_dec = i[41]
-        b_rec.save()
+# from .models import *
+
+# mycursor = mydb.cursor()
+# def porter_in(request):
+#     col = 0
+#     for t in range(len(tablenames)):
+#         mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+tablenames[t]+"';")
+#         for c in range(mycursor.fetchone()[0]):
+#             mycursor.execute("SELECT " + columnnames[col] + " FROM "+tablenames[t]+";")
+#             result=mycursor.fetchall()
+#             for x in result:
+#                 alltables[t][c].append(x[0])
+#             col+=1
+    
+#     current_table = rearrange('systemuser')#change string parameter to desired table name reference in tablenames above
+#     porter_out(current_table)
+#     return render(request, "home.html")
+
+# def rearrange(var):
+#     arr = []
+#     mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+var+"';")
+#     cols = mycursor.fetchone()[0]
+#     mycursor.execute("SELECT count(*) FROM "+var+";")
+#     rows = mycursor.fetchone()[0]
+#     for r in range(rows):
+#         inner = []
+#         for c in range(cols):
+#             inner.append(globals()[var][c][r])
+#         arr.append(inner)
+
+#     return arr
+
+    
+# def porter_out(table):
+#     sys_user = SystemUsers()
+#     for i in table:
+#         sys_user.userid = i[0]
+#         sys_user.password = i[1]
+#         sys_user.su_firstname = i[2]
+#         sys_user.su_middlename = i[3]
+#         sys_user.su_mobilenumber = i[4]
+#         sys_user.su_lastname = i[5]
+#         sys_user.su_emailaddress = i[6]
+#         sys_user.usertype = i[7]
+#         sys_user.su_profilepic = i[8]
+#         sys_user.approver_flag = i[9]
+#         sys_user.save()
+
+
+#-----------------------------------------------#
+
+
+# mycursor = mydb.cursor()
+# def porter_in(request):
+#     col = 0
+#     for t in range(len(tablenames)):
+#         mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+tablenames[t]+"';")
+#         for c in range(mycursor.fetchone()[0]):
+#             mycursor.execute("SELECT " + columnnames[col] + " FROM "+tablenames[t]+";")
+#             result=mycursor.fetchall()
+#             for x in result:
+#                 alltables[t][c].append(x[0])
+#             col+=1
+    
+#     current_table = rearrange('ratestable')#change string parameter to desired table name reference in tablenames above
+#     porter_out(current_table)
+#     return render(request, "home.html")
+
+# def rearrange(var):
+#     arr = []
+#     mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+var+"';")
+#     cols = mycursor.fetchone()[0]
+#     mycursor.execute("SELECT count(*) FROM "+var+";")
+#     rows = mycursor.fetchone()[0]
+#     for r in range(rows):
+#         inner = []
+#         for c in range(cols):
+#             inner.append(globals()[var][c][r])
+#         arr.append(inner)
+
+#     return arr
+
+    
+# def porter_out(table):
+#     rt = RatesTable()
+#     for i in table:
+#         rt.minReading = i[0]
+#         rt.minReadingCharge = i[1]
+#         rt.rateAfterMin = i[2]
+#         rt.ratePenalty = i[3]
+#         rt.ratePenaltyFreq = i[4]
+
+
+
+
+
--- wb2/index/views.py
+++ wb2/index/views.py
@@ -1239,6 +1239,54 @@ def porter_out(table):
         b_rec.total_paid_dec = i[40]
         b_rec.total_usage_dec = i[41]
         b_rec.save()
+
+#------------------JAZZY<3----------------------#
+
+mycursor = mydb.cursor()
+def porter_in(request):
+    col = 0
+    for t in range(len(tablenames)):
+        mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+tablenames[t]+"';")
+        for c in range(mycursor.fetchone()[0]):
+            mycursor.execute("SELECT " + columnnames[col] + " FROM "+tablenames[t]+";")
+            result=mycursor.fetchall()
+            for x in result:
+                alltables[t][c].append(x[0])
+            col+=1
+    
+    current_table = rearrange('ratestable')#change string parameter to desired table name reference in tablenames above
+    porter_out(current_table)
+    return render(request, "home.html")
+
+def rearrange(var):
+    arr = []
+    mycursor.execute("SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME = '"+var+"';")
+    cols = mycursor.fetchone()[0]
+    mycursor.execute("SELECT count(*) FROM "+var+";")
+    rows = mycursor.fetchone()[0]
+    for r in range(rows):
+        inner = []
+        for c in range(cols):
+            inner.append(globals()[var][c][r])
+        arr.append(inner)
+
+    return arr
+
+    
+def porter_out(table):
+    rt = Rates()
+    for i in table:
+        rt.minReading = i[0]
+        rt.minReadingCharge = i[2]
+        rt.rateAfterMin = i[3]
+        rt.ratePenalty = i[4]
+        rt.ratePenaltyFreq = i[5]
+        rt.save()
+
+
+#-----------------------------------#
+
+        
 def login(request):
     if request.method == "POST":
         username = request.POST['username']
