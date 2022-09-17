dab855ce6af26b4c6e0a80153457b74b280b68bc
chrlhjra <chijara68@gmail.com>
2022-09-17 11:18:38 +0800

    f

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -1239,6 +1239,7 @@ def porter_out(table):
         b_rec.total_paid_dec = i[40]
         b_rec.total_usage_dec = i[41]
         b_rec.save()
+        
 def login(request):
     if request.method == "POST":
         username = request.POST['username']
