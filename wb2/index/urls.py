b263c3df99430e72f92d573909cabc57430a30fc
Patrickkk14 <113079690+Patrickkk14@users.noreply.github.com>
2022-09-19 16:56:15 -0400

    09-19-2022

--- wb2/index/urls.py
+++ wb2/index/urls.py
@@ -8,6 +8,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.landingpage,name='lp'),
     path('login',views.login,name='login'),
-    path('user_creation', views.user_creation,name='user_creation')
-    
+    path('user_creation', views.user_creation,name='user_creation'),
 ]
