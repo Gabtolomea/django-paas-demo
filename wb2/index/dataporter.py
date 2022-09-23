a9e439a079e3dab1836738e4e99908bf43e20ac1
charliegwapo <91324862+chrlhjra@users.noreply.github.com>
2022-09-23 13:37:19 +0800

    fa

--- wb2/index/dataporter.py
+++ wb2/index/dataporter.py
@@ -292,7 +292,7 @@ def porter_out(tables):
         usage_rec.txrefnum_dec=	i[161-1]
         usage_rec.ior_dec	=i[162-1]
         arr= i[175].split('-')
-        usage_rec.consumerid = ConsumerInfo.objects.get(consumer_id=i[163-1])   
+        usage_rec.consumerid = ConsumerInfo.objects.get(consumer_id=arr[0])   
         usage_rec.amountpaid_str_apr =	i[164-1]
         usage_rec.amountpaid_str_aug = i[165-1]
         usage_rec.amountpaid_str_dec = i[166-1]
