bb208a65663b2fdd63ab83c39490da535bac27f0
charliegwapo <91324862+chrlhjra@users.noreply.github.com>
2022-09-27 11:33:30 +0800

    population

--- wb2/index/urls.py
+++ wb2/index/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"),name="password_reset_confirm"),
     path('consumercreation', views.consumercreation,name='consumercreation'),
     path('bills_list/', views.bills_list, name='bills_list'),
-    path('consumer_list/', views.consumer_list, name='consumer_list')
+    path('consumer_list/', views.consumer_list, name='consumer_list'),
     path('inputreading', views.inputreading, name='inputreading')
     # path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"),name="password_reset_complete"),
 
