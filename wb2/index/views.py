5592446ba9d74f30885280ded265814e272aa8c3
charliegwapo <91324862+chrlhjra@users.noreply.github.com>
2022-10-04 09:12:37 +0800

    rq

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -24,13 +24,10 @@ from .dataporter import *
 from .ledger import *
 import math
 from .tokens import generate_token
-<<<<<<< HEAD
 from django.core.files.storage import FileSystemStorage
-=======
 from django.db.models import F, Sum
 from django.db.models.functions import Coalesce
 
->>>>>>> ryla
 
 def porter(request):
     # porter_in()
