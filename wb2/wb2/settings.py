a16b9de33de75b250b2b1f79b842816f14d09a2c
jazferlex <79888058+jazferlex@users.noreply.github.com>
2022-10-10 13:05:39 +0800

    asdfsf

--- wb2/wb2/settings.py
+++ wb2/wb2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v1#y52o##fi-heo-bh8@!*g)olfw9c75viwsxga81lvxho1fm('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['waterbilling.test']
 
 
 # Application definition
