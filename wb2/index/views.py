e11dd24bd7efb868ecd48fbf5ffe5ecacb60e4a5
Rena Lequin <113079463+renalequinn@users.noreply.github.com>
2022-10-18 13:31:31 +0800

    afsfd

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -112,7 +112,7 @@ def user_creation(request):
             user.authorizedapprover = authorizedapprover
             user.profilepic = profilepic
             user.save()
-            return redirect('dashboard')
+            return redirect('sysuser')
     context = {
         'form': form,
         'errors': form.errors,
@@ -663,7 +663,7 @@ def user_edit(request, id):
             fss.save(upload.name, upload)
             # sys.profilepic = pic
             sys.save()
-
+        messages.success(request, 'Successfully Submitted!')
         return redirect('sysuser')
     context = {
         'sys': sys,
