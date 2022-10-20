ccb98a0ab3381a6f2dfd76a9c2e88261110254ee
rylabdumamag <91299883+rylabdumamag@users.noreply.github.com>
2022-10-20 17:00:16 +0800

    Life could be a dream

--- wb2/index/forms.py
+++ wb2/index/forms.py
@@ -146,9 +146,9 @@ class Userinfoupdate(ModelForm):
 
 class ConscumertypecreationForm (ModelForm):
     contype = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    minReading = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    minReadingCharge = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    rateAfterMin = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    minReading = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
+    minReadingCharge = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
+    rateAfterMin = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
     added_by = forms.ModelChoiceField(widget=forms.Select(attrs={'class': 'form-control'}),queryset=SystemUsers.objects.all())
 
     class Meta():
