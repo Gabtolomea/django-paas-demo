7c96ab0ccf4fbae85fbdc988be9e23c4d461da3b
jazferlex <jazfer.licaros@ctu.edu.ph>
2022-12-07 16:55:37 +0800

    asdfasdf

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -895,18 +895,6 @@ def view_barangay(request, id):
     }
     return render(request, 'view_barangay.html', context)
 
-
-def unsettled_bill(request):
-    ub = ConsumerInfo.objects.all()
-    year = datetime.today().year
-    print(year)
-    context = {
-        'year': year,
-        'ub': ub
-    }
-    return render(request, 'unsettled_bill.html', context)
-
-
 def usage_report_data(request, year):
     
     years = []
@@ -1059,21 +1047,14 @@ def unsettled_bills_p(request, p):
         def __init__(self, y, u):
             self.y = y
             self.u = u
-    tb =ConsumerInfo.objects.all().aggregate(tots = Sum('current_bal'))
     pages = int(p)
     ubs = ConsumerInfo.objects.filter(current_bal__gt=0).order_by('lastname', 'firstname', 'middlename')
-    
+    tb = ubs.aggregate(tots = Sum('current_bal'))
     table = []
     for j in ubs:
-        alltran = Transactions.objects.filter(acctID_id=j.consumer_id, transType='Billing')
-        years = []
-        for i in alltran:
-            if i.year not in years:
-                if i.year is not None:
-                    years.append(i.year)
-        years.reverse()
-        if years:
-            a = years[0]
+        alltran = Transactions.objects.filter(acctID_id=j.consumer_id, transType='Billing').order_by('year')
+        if alltran[0].year is not None:
+            a = alltran[0].year
         else:
             a = 0
         yeah = ub_year(a,j)
@@ -1139,6 +1120,7 @@ def view_unsettled_bills(request, id, year):
         total_bill = 0
         reading_date = ''
         total_amount_paid = 0
+
         if c < pcount and pcount != 0:
             if i == payment[c].month:
                 total_amount_paid = payment[c].payment
@@ -1150,13 +1132,14 @@ def view_unsettled_bills(request, id, year):
                 reading_date = billing[j].date
                 total_bill = billing[j].bill
                 j += 1
-                a = view_utang(month, reading, reading_date, usage,
-                       total_bill, total_amount_paid)
-        table.append(a)
-    context = {'uv': uv,
-               'years': years,
-               'current': year,
-               'table': table, }
+                a = view_utang(month, reading, reading_date, usage,total_bill, total_amount_paid)
+                table.append(a)
+    context = {
+        'uv': uv,
+        'years': years,
+        'current': year,
+        'table': table, 
+    }
     return render(request, 'view_unsettled_bills.html', context)
 
 
