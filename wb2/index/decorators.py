36859b25ba88724df9d1515f466ee0772b1960f9
rylabdumamag <91299883+rylabdumamag@users.noreply.github.com>
2023-01-04 14:51:28 +0800

    Is this it? or is it it?

--- wb2/index/decorators.py
+++ wb2/index/decorators.py
@@ -13,15 +13,15 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
     return wrapper_func
 
-def allowed_users(allowed_roles=[]):
-	def decorator(view_func):
-		def wrapper_func(request, *args, **kwargs):
-			u_type = None
-			if request.user.u_type is not None:
-				u_type = request.user.u_type
-			if u_type in allowed_roles:
-				return view_func(request, *args, **kwargs)
-			else:
-				return HttpResponse('You are not authorized to view this page')
-		return wrapper_func
-	return decorator
+# def allowed_users(allowed_roles=[]):
+# 	def decorator(view_func):
+# 		def wrapper_func(request, *args, **kwargs):
+# 			u_type = None
+# 			if request.user.u_type is not None:
+# 				u_type = request.user.u_type
+# 			if u_type in allowed_roles:
+# 				return view_func(request, *args, **kwargs)
+# 			else:
+# 				return HttpResponse('You are not authorized to view this page')
+# 		return wrapper_func
+# 	return decorator
