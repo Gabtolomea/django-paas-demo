d30750efcc3dcdef17068695c78a6761c93a6fee
jazfer <jazfer.licaros@ctu.edu.ph>
2023-01-18 12:59:40 +0800

    THrwwe

--- wb2/index/dataporter.py
+++ wb2/index/dataporter.py
@@ -30,13 +30,13 @@ alltables = [
 
 sorted_tables = []
 
-mydb = mysql.connector.connect(
-   host="localhost",
-   user="root",
-   password="yjh434ctuG@-@",
-   database="lgu_ginatilan_db"
-)
-mycursor = mydb.cursor()
+# mydb = mysql.connector.connect(
+#    host="localhost",
+#    user="root",
+#    password="yjh434ctuG@-@",
+#    database="lgu_ginatilan_db"
+# )
+# mycursor = mydb.cursor()
 
 def porter_in():
     col = 0
