20a9a704e3960271c9a41642f8a141dd36535c4e
chrlhjra <chijara68@gmail.com>
2023-02-12 00:53:46 +0800

    fa

---
+++ waterbilling2.0/wb2/index/functions.py
@@ -0,0 +1,147 @@
+from base64 import urlsafe_b64decode
+
+from django.forms import ValidationError
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+import math
+import random
+import string
+from datetime import datetime
+def n_int(var):
+    if var is None:
+        return 0
+    else:
+        return var
+
+def n_str(var):
+    if var is None:
+        return ''
+    else:
+        return var
+
+def str_int(var):
+    if var == '':
+        return 0
+    else:
+        return int(var)
+def camelize():
+    cons = ConsumerInfo.objects.all()
+    for c in cons:
+        c.firstname = c.firstname.title()
+        c.lastname = c.lastname.title()
+        c.middlename = c.middlename.title()
+        c.save()
+def enye(cons):
+    for c in cons:
+        chars = ["ã‘","ã±"]
+        for char in chars:
+            if char in c.firstname:
+                c.firstname = c.firstname.replace(char, "ñ")
+            if char in c.lastname:
+                c.lastname = c.lastname.replace(char, "ñ")
+            if char in c.middlename:
+                c.middlename = c.middlename.replace(char, "ñ")
+            c.save()
+def capitalize():
+    cons = ConsumerInfo.objects.all()
+    for c in cons:
+        c.firstname = c.firstname.upper()
+        c.lastname = c.lastname.upper()
+        c.middlename = c.middlename.upper()
+        c.save()
+def years(id):
+    years = []
+    alltrans = Transactions.objects.filter(acctID=id, transType='Billing') | Transactions.objects.filter(acctID=id, transType='Reset Meter')
+    for i in alltrans:
+        if i.date.year not in years:
+            years.append(i.date.year)
+    if datetime.today().year not in years:
+        years.append(datetime.today().year)
+    return years
+def last_reading(id, year, mo):
+    allyears = years(id)
+    allyears.sort(reverse=True)
+    fyears = [item for item in allyears if item <= year]
+    for i in fyears:
+        for j in reversed(range(1,mo)):
+            try:
+                lasttran = Transactions.objects.get(acctID=id, transType='Billing', year=i, month=j)
+                lastreading = lasttran.meterReading
+                return lastreading
+            except ObjectDoesNotExist:
+                pass
+        mo = 13
+    return 0
+
+def create_brec(address_id, year):
+    con_b_rec = BarangayRecord()  
+    con_b_rec.barangayrec_id = f"{address_id}-{year}"
+    con_b_rec.barangaycode = Barangays.objects.get(id=address_id)
+    con_b_rec.year = year
+    con_b_rec.total_due_jan = 0
+    con_b_rec.total_due_feb = 0
+    con_b_rec.total_due_mar = 0
+    con_b_rec.total_due_apr = 0
+    con_b_rec.total_due_may = 0
+    con_b_rec.total_due_jun = 0
+    con_b_rec.total_due_jul = 0
+    con_b_rec.total_due_aug = 0
+    con_b_rec.total_due_sept = 0
+    con_b_rec.total_due_oct = 0
+    con_b_rec.total_due_nov = 0
+    con_b_rec.total_due_dec = 0
+    con_b_rec.total_paid_jan = 0
+    con_b_rec.total_paid_feb = 0
+    con_b_rec.total_paid_mar = 0
+    con_b_rec.total_paid_apr = 0
+    con_b_rec.total_paid_may = 0
+    con_b_rec.total_paid_jun = 0
+    con_b_rec.total_paid_jul = 0
+    con_b_rec.total_paid_aug = 0
+    con_b_rec.total_paid_sept = 0
+    con_b_rec.total_paid_oct = 0
+    con_b_rec.total_paid_nov = 0
+    con_b_rec.total_paid_dec = 0
+    con_b_rec.total_usage_jan = 0
+    con_b_rec.total_usage_feb = 0
+    con_b_rec.total_usage_mar = 0
+    con_b_rec.total_usage_apr = 0
+    con_b_rec.total_usage_may = 0
+    con_b_rec.total_usage_jun = 0
+    con_b_rec.total_usage_jul = 0
+    con_b_rec.total_usage_aug = 0
+    con_b_rec.total_usage_sept = 0
+    con_b_rec.total_usage_oct = 0
+    con_b_rec.total_usage_nov = 0
+    con_b_rec.total_usage_dec = 0
+    return con_b_rec
+def get_balance(id):
+    user = ConsumerInfo.objects.get(consumer_id = id)
+    trans = Transactions.objects.filter(acctID = id)
+    asc_trans = trans.order_by('year', 'month','transactionid')
+    bal = 0
+    for i in range(len(asc_trans)):
+        if asc_trans[i].transType == 'Billing':
+            bal+=asc_trans[i].bill
+        elif asc_trans[i].transType == 'Payment':
+            bal-=asc_trans[i].payment
+        elif asc_trans[i].transType == 'Penalty':
+            bal+=asc_trans[i].bill
+        elif asc_trans[i].transType == 'Discount':
+            bal-=asc_trans[i].payment
+    user.current_reading = trans.filter(transType='Billing').order_by('-year', '-month')[0].meterReading
+    user.current_bal = math.ceil(bal*100)/100
+    user.save()
+
+
+def gen_token():
+    nums = [str(x) for x in range(10)]
+    alphabet = list(string.ascii_lowercase)
+    choices = nums + alphabet
+    token = ""
+    d = datetime.now().timestamp()
+    random.seed(d)
+    for i in range(10):
+        char = random.choice(choices)
+        token+=(char)
+    return token
---
+++ waterbilling2.0/wb2/index/models.py
@@ -0,0 +1,202 @@
+from datetime import date, datetime
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
+# Create your models here.
+
+class LoginRec(models.Model):
+    username = models.CharField(max_length=20,primary_key = True)
+    token = models.CharField(max_length = 10)
+    last_access = models.DateTimeField()
+    expiration = models.DateTimeField(default = datetime.now())
+
+
+    
+
+class SystemUsers(AbstractUser):
+    first_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20, blank=True)
+    username = models.CharField(max_length=20,primary_key = True)
+    is_admin = models.BooleanField(default=False)
+    is_teller = models.BooleanField(default=False)
+    is_supervisor = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    is_reader = models.BooleanField(default=False)
+    mid_name = models.CharField(max_length=20, blank=True)
+    mobilenum = models.CharField(max_length=20, blank=True)
+    authorizedapprover = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100,null=True, blank=True)
+    profilepic = models.ImageField(upload_to= '', blank=True, null=True, default='profile12.png')
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        SIZE = 300, 300
+        if self.profilepic:
+            pic = Image.open(self.profilepic.path)
+            pic.thumbnail(SIZE, Image.LANCZOS)
+            pic.save(self.profilepic.path)
+
+    
+    def __str__(self) -> str:
+        return self.username
+    
+
+class ConsumerType(models.Model):
+    contypeid = models.CharField(primary_key=True, max_length=20)
+    contype = models.CharField(max_length=20)
+    minReading = models.IntegerField()
+    minReadingCharge = models.IntegerField()
+    rateAfterMin = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    date_mod = models.DateField(auto_now=True)
+    added_by = models.ForeignKey(SystemUsers, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.contype
+
+class Penalty(models.Model):
+    penaltycode = models.CharField(primary_key=True, max_length=20)
+    penalty_after =  models.IntegerField(default = 0)#months
+    penalty_rate = models.FloatField(default = 0)
+    penalty_info = models.TextField(max_length=300, blank=True, null=True)
+    date_added = models.DateField(auto_now_add=True, null=True)
+    daysappliedafter = models.IntegerField(default=0)
+    added_by = models.ForeignKey(SystemUsers, on_delete=models.SET_NULL, null=True)
+    def __str__(self) -> str:
+        return self.penaltycode
+
+
+class Discount(models.Model):
+    discountcode = models.CharField(primary_key=True, max_length=20)
+    discount_rate = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    added_by = models.ForeignKey(SystemUsers, on_delete=models.SET_NULL, null=True)
+
+class Barangays(models.Model):
+    barangay = models.CharField(max_length=20, blank=True)
+    def __str__(self) -> str:
+        return self.barangay
+class BarangayRecord(models.Model):
+    barangayrec_id = models.CharField(primary_key = True ,max_length=20)
+    barangaycode = models.ForeignKey(Barangays,on_delete=models.SET_NULL, null=True)
+    year = models.IntegerField()
+    total_due_jan = models.IntegerField(default=0)
+    total_paid_jan = models.IntegerField(default=0)
+    total_usage_jan = models.IntegerField(default=0)
+    total_due_feb = models.IntegerField(default=0)
+    total_paid_feb = models.IntegerField(default=0)
+    total_usage_feb = models.IntegerField(default=0)
+    total_due_mar = models.IntegerField(default=0)
+    total_paid_mar = models.IntegerField(default=0)
+    total_usage_mar = models.IntegerField(default=0)
+    total_due_apr = models.IntegerField(default=0)
+    total_paid_apr = models.IntegerField(default=0)
+    total_usage_apr = models.IntegerField(default=0)
+    total_due_may = models.IntegerField(default=0)
+    total_paid_may = models.IntegerField(default=0)
+    total_usage_may = models.IntegerField(default=0)
+    total_due_jun = models.IntegerField(default=0)
+    total_paid_jun = models.IntegerField(default=0)
+    total_usage_jun = models.IntegerField(default=0)
+    total_due_jul = models.IntegerField(default=0)
+    total_paid_jul = models.IntegerField(default=0)
+    total_usage_jul = models.IntegerField(default=0)
+    total_due_aug = models.IntegerField(default=0)
+    total_paid_aug = models.IntegerField(default=0)
+    total_usage_aug = models.IntegerField(default=0)
+    total_due_sept = models.IntegerField(default=0)
+    total_paid_sept = models.IntegerField(default=0)
+    total_usage_sept = models.IntegerField(default=0)
+    total_due_oct = models.IntegerField(default=0)
+    total_paid_oct = models.IntegerField(default=0)
+    total_usage_oct = models.IntegerField(default=0)
+    total_due_nov = models.IntegerField(default=0)
+    total_paid_nov = models.IntegerField(default=0)
+    total_usage_nov = models.IntegerField(default=0)
+    total_due_dec = models.IntegerField(default=0)
+    total_paid_dec = models.IntegerField(default=0)
+    total_usage_dec = models.IntegerField(default=0)
+
+
+#Consumer Creation
+class ConsumerInfo(models.Model):
+    consumer_id = models.CharField(primary_key=True, max_length=15)
+    meternumber = models.CharField(max_length=20, blank=True, null=True)
+    firstname = models.CharField(max_length=50, blank=True)
+    lastname = models.CharField(max_length=50, blank=True)
+    middlename = models.CharField(max_length=50, blank=True)
+    homeaddress = models.CharField(max_length=50, blank=True)
+    installation_address = models.ForeignKey(Barangays, on_delete=models.CASCADE)
+    initialmeterreading = models.IntegerField()
+    contypeid = models.ForeignKey(ConsumerType,on_delete=models.CASCADE)#consumertype
+    status = models.IntegerField()
+    penaltycounter = models.IntegerField(null=True)
+    stopmeterflag = models.BooleanField()
+    deleteflag = models.BooleanField()
+    disconnectionflag = models.BooleanField()
+    mobilenum = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=100,null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    sex = models.CharField(max_length=6,null=True, blank=True)
+    sitio = models.CharField(max_length=100,null=True, blank=True)
+    picture = models.ImageField(null=True, blank=True)
+    current_bal = models.FloatField(default=0)
+    current_reading = models.IntegerField(default=0)
+    cummulative = models.FloatField(default=0)
+    date_added = models.DateField(auto_now_add=True, null=True)
+    excess = models.IntegerField(default=0)
+    penaltycode = models.ForeignKey(Penalty, on_delete= models.SET_NULL, null=True, default='POO1')
+class Transactions(models.Model):
+    TRANS_TYPE = (
+        ('Billing','Billing'),
+        ('Payment','Payment'),
+        ('Penalty','Penalty'),
+        ('Discount','Discount'),
+    )
+    transactionid = models.AutoField(primary_key=True)
+    date = models.DateField(null=True, blank=True)
+    acctID = models.ForeignKey(ConsumerInfo, on_delete=models.CASCADE)
+    transType = models.CharField(max_length=20, choices=TRANS_TYPE)
+    meterReading = models.IntegerField(blank=True, null=True)
+    prevReading = models.IntegerField(blank=True, null=True)
+    usage = models.IntegerField(blank=True, null=True)
+    contypeid = models.CharField(max_length=20, blank=True, null=True)#consumertype
+    penaltyCode = models.ForeignKey(Penalty, on_delete= models.SET_NULL, null=True)
+    discountcode = models.CharField(max_length=50, null=True)
+    bill = models.FloatField(null=True)
+    is_billpaid = models.BooleanField(default = False)
+    month = models.IntegerField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    receivedamt = models.FloatField(null=True)
+    payment = models.FloatField(null=True)
+    processedBy = models.CharField(max_length=50, null=True)
+    or_number = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return str(self.transactionid)
+
+class revenuecode(models.Model):
+    application_fee = models.FloatField(default = 0)
+    mayors_permit = models.FloatField(default = 0)
+    gravel_excavation = models.FloatField(default = 0)
+    asphalted_road = models.FloatField(default = 0)
+    cemented_road = models.FloatField(default = 0)
+    additionalfee_pipe_of_20_lineal_feet = models.FloatField(default = 0)
+    residentialservice_per_month = models.FloatField(default = 0)
+    commercialservice_per_month = models.FloatField(default = 0)
+    residentialservice_excess_per_cubicmeter = models.FloatField(default = 0)
+    commercialservice_excess_per_cubicmeter = models.FloatField(default = 0)
+    drilling_from_mainline = models.FloatField(default = 0)
+    reinstallation_fee = models.FloatField(default = 0)
+    tapping_fee = models.FloatField(default = 0)
+    repair_fee = models.FloatField(default = 0)
+    transfer_fee = models.FloatField(default = 0)
+    three_month_penalty = models.FloatField(default = 0)
+    send_disconnection_notice_after = models.IntegerField(default = 0)#months
+    disconnection_after = models.IntegerField(default = 0)#months
+    penalty_after = models.IntegerField(default = 0)#months
+    fix_amount_penalty = models.FloatField(default = 0)
+    percentage_penalty = models.FloatField(default = 0)
+
