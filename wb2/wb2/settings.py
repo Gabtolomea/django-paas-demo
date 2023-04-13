6f7f7b4f0e9d3f59ec697a26701782ee5270fdb7
jazferlex <79888058+jazferlex@users.noreply.github.com>
2023-04-13 08:46:33 +0800

    ewrwerewr

--- wb2/wb2/settings.py
+++ wb2/wb2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v1#y52o##fi-heo-bh8@!*g)olfw9c75viwsxga81lvxho1fm(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['waterbilling.test', '127.0.0.1', '192.168.1.109', '172.28.52.89',]
+ALLOWED_HOSTS = ['waterbilling.test', '127.0.0.1', '192.168.1.109', '172.28.52.89','172.28.20.129',' ']
 
 # Application definition
 
