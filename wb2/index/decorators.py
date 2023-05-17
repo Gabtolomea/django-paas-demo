766980d39b8741291ed3383d1dacb78fefe688c3
jazferlex <79888058+jazferlex@users.noreply.github.com>
2023-05-17 10:25:35 +0800

    ASDAD

--- wb2/index/decorators.py
+++ wb2/index/decorators.py
@@ -2,7 +2,7 @@
 from django.shortcuts import redirect
 from .DBdb import *
 from .models import *
-from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 
 
 def unauthenticated_user(view_func):
@@ -13,15 +13,24 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
     return wrapper_func
 
-# def allowed_users(allowed_roles=[]):
-# 	def decorator(view_func):
-# 		def wrapper_func(request, *args, **kwargs):
-# 			u_type = None
-# 			if request.user.u_type is not None:
-# 				u_type = request.user.u_type
-# 			if u_type in allowed_roles:
-# 				return view_func(request, *args, **kwargs)
-# 			else:
-# 				return HttpResponse('You are not authorized to view this page')
-# 		return wrapper_func
-# 	return decorator
+def allowed_users(allowed_roles=[]):
+	def decorator(view_func):
+		def wrapper_func(request, *args, **kwargs):
+			proceed = False
+			roles = [
+				'admin',
+				'teller',
+				'supervisor',
+                'manager',
+                'reader'
+            ]
+			user = SystemUsers.objects.get(username=request.user)
+			for i in roles:
+				if user.__dict__[f'is_{i}'] and i in allowed_roles:
+					proceed = True
+			if proceed:
+				return view_func(request, *args, **kwargs)
+			else:
+				return HttpResponse('You are not authorized to view this page')
+		return wrapper_func
+	return decorator
