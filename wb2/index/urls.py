0a1faa7765eb99a17ce2a061007f53bdc0424333
jazferlex <79888058+jazferlex@users.noreply.github.com>
2023-05-17 09:28:27 +0800

    asdfsdff

--- wb2/index/urls.py
+++ wb2/index/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('reports/view_barangay/<id>/', views.view_barangay, name='view_barangay'),
     path('reports/usage_report_data/<int:year>', views.usage_report_data, name='usage_report_data'),
     path('reports/revenue_report/<int:year>', views.revenue_report, name='revenue_report'),
+    path('reports/consumption/<int:year>', views.consumption, name='consumption'),
     path('unsettled_bills',views.unsettled_bills, name='unsettled_bills'),
     path('unsettled_bills/<id>/<int:year>',views.view_unsettled_bills, name='view_unsettled_bills'),
     path('payment/<id>/', views.payment, name='payment'),
