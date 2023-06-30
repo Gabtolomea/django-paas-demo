ef7da0d844dff285a64339846cba68af37e455aa
chrlhjra <91324862+chrlhjra@users.noreply.github.com>
2023-06-30 10:45:15 +0800

    sdaf

--- wb2/index/views.py
+++ wb2/index/views.py
@@ -2698,22 +2698,8 @@ def consumption(request, year):
     return render(request,'consumption.html', context)
 
 def exemption(request):
-    search = request.GET.get('search')
-    isnum = search.isnumeric() if search else False
-    cons = []
-
-    if search:
-        if isnum:
-            cons = ConsumerInfo.objects.filter(Q(meternumber=search) | Q(consumer_id__icontains=search), deleteflag=0).order_by('lastname', 'firstname', 'middlename')
-        else:
-            cons = ConsumerInfo.objects.filter(Q(firstname__icontains=search) | Q(middlename__icontains=search) | Q(lastname__icontains=search) | Q(homeaddress__icontains=search), deleteflag=0).order_by('lastname', 'firstname', 'middlename')
-    else:
-        search = None  # Set search to None when there is no search query
-    print(cons)
+    pass
     context = {
-        'cons': cons,
-        'is_exemption': True,
-        'search': search,  # Pass the search query to the context
+        
     }
-
     return render(request, 'exemption.html', context)
