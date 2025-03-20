9c0edc940d21e279396d67d06e426f8850082ed7
Kathrina D. Bandajon <151318955+lnnngfjdd@users.noreply.github.com>
2025-03-20 14:50:11 +0800

    Added Monthly Bill Reports

--- wb2/index/urls.py
+++ wb2/index/urls.py
@@ -65,8 +65,7 @@ urlpatterns = [
     path('resolve_issue', views.resolve_issue,name='resolve_issue'),
     path('new_penalty', views.new_penalty), #added by K. Bandajon October 29, 2024 The new penalty rate-.- ID: NewPenaltyOct2024
     path('mark_unpaid', views.mark_unpaid, name ='mark_unpaid'),# added Enjambre & Sobrian 26/11/2024 for unpaid in payment history
-    path('record_list', views.record_list, name ='record_list') #added by kathrina D. Bandajon nov. 26, 2024 -.- ID: AllPaid2611
-
-    #path 'unpaid',
+    path('record_list', views.record_list, name ='record_list'), #added by kathrina D. Bandajon nov. 26, 2024 -.- ID: AllPaid2611
+    path("billing-report/", views.monthly_billing_report, name="billing_report"),
 ]
 
