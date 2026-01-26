0379960453d7797e789799e63e05605958c345e9
Chito_ace <quinanolarichitojr@gmail.com>
2026-01-26 11:26:34 +0800

    UPDATED UI 1/26/2026

--- wb2/index/functions.py
+++ wb2/index/functions.py
@@ -703,11 +703,11 @@ def dump_database():
     # )
     #Changed by Bandajon k., 
     cnx = MySQLdb.connect(
-        user='root',
-        password='jeizel112903',
-        host='127.0.0.1',
+        user='wbilling',
+        password='@-@JazfeR123',
+        host='192.168.0.167',
         port=3306,
-        db='waterbillingv3',
+        db='wb2',
         charset='latin1'
     )
 
