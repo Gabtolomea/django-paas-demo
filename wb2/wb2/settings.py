82e251fe77aaad40ca278970cd1bdd027e5c2f1e
Gabriel Tolomea <gtolomea@gmail.com>
2026-09-20 22:11:48 +0800

    Fix correct requirements.txt used by Railway build

--- wb2/wb2/settings.py
+++ wb2/wb2/settings.py
@@ -125,11 +125,17 @@ DATABASES = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_USE_TLS = True
+
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ginatilancebuwater'
-SERVER_EMAIL = 'ginatilancebuwater'
-EMAIL_HOST_PASSWORD = 'axsrouetdxposfko'
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+
+SERVER_EMAIL = os.environ.get('SERVER_EMAIL', '')
+
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+
 EMAIL_PORT = 587
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 30240
